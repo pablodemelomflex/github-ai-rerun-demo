929e62df721ee152504a162d5b3b70cb16f8fde8
Pablo de Melo <pablo.demelo.mflex@gmail.com>
2026-06-11 17:23:22 -0300

    deleted tests from this repo

--- tests/subtract.test.ts
+++
@@ -1,6 +0,0 @@
-import { subtract } from "../src/subtract";
-import { test, expect } from "@jest/globals";
-
-test("subtract works", () => {
-  expect(subtract(5, 3)).toBe(2);
-});
--- tests/sum.test.ts
+++
@@ -1,7 +0,0 @@
-// tests/sum.test.ts
-import { sum } from "../src/sum";
-import { test, expect } from "@jest/globals";
-
-test("sum works", () => {
-  expect(sum(2, 3)).toBe(5);
-});
